35b5a5178b576080fe34acb86192f57164832fe0
Luis Floreani <luis.floreani@gmail.com>
2014-12-02 14:04:36 -0300

    more docs on headers

--- lib/M2XStream.m
+++ lib/M2XStream.m
@@ -8,12 +8,20 @@
 
 #import "M2XStream.h"
 #import "M2XDevice.h"
+#import "NSDate+M2X.h"
 
 @interface M2XStream()
 @property (nonatomic, strong) M2XDevice *device;
 @end
 
 @implementation M2XStream
+
++ (void)fetchWithClient:(M2XClient *)client device:(M2XDevice *)device name:(NSString *)name completionHandler:(M2XStreamCallback)completionHandler {
+    [client getWithPath:[NSString stringWithFormat:@"%@/streams/%@", device.path, name] parameters:nil completionHandler:^(M2XResponse *response) {
+        M2XStream *stream = [[M2XStream alloc] initWithClient:client device:device attributes:response.json];
+        completionHandler(stream, response);
+    }];    
+}
 
 + (void)listWithClient:(M2XClient *)client device:(M2XDevice *)device completionHandler:(M2XArrayCallback)completionHandler {
     [client getWithPath:[NSString stringWithFormat:@"%@/streams", device.path] parameters:nil completionHandler:^(M2XResponse *response) {
@@ -54,8 +62,36 @@
     }];
 }
 
+- (void)samplingWithParameters:(NSDictionary *)parameters completionHandler:(M2XArrayCallback)completionHandler {
+    [self.client getWithPath:[NSString stringWithFormat:@"%@/sampling", [self path]] parameters:parameters completionHandler:^(M2XResponse *response) {
+        completionHandler(response.json[@"values"], response);
+    }];
+}
+
+- (void)statsWithParameters:(NSDictionary *)parameters completionHandler:(M2XBaseCallback)completionHandler {
+    [self.client getWithPath:[NSString stringWithFormat:@"%@/stats", [self path]] parameters:parameters completionHandler:^(M2XResponse *response) {
+        completionHandler(response);
+    }];
+}
+
+- (void)updateValue:(NSNumber *)value timestamp:(NSString *)timestamp completionHandler:(M2XBaseCallback)completionHandler {
+    NSDictionary *params;
+    if (!timestamp) {
+        params = @{@"value": value};
+    } else {
+        params = @{@"value": value, @"timestamp": timestamp};
+    }
+    
+    [self.client putWithPath:[NSString stringWithFormat:@"%@/value", [self path]] parameters:params completionHandler:completionHandler];
+}
+
 - (void)postValues:(NSArray *)values completionHandler:(M2XBaseCallback)completionHandler {
     [self.client postWithPath:[NSString stringWithFormat:@"%@/values", [self path]] parameters:@{@"values": values} completionHandler:completionHandler];
+}
+
+- (void)deleteValuesFrom:(NSString *)start to:(NSString *)stop completionHandler:(M2XBaseCallback)completionHandler {
+    NSDictionary *params = @{@"from": start, @"end": stop};
+    [self.client deleteWithPath:[NSString stringWithFormat:@"%@/values", [self path]] parameters:params completionHandler:completionHandler];
 }
 
 - (NSString *)path {
