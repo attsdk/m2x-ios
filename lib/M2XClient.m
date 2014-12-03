968bcbb17058119765c9736259303fbc7c45648a
Luis Floreani <luis.floreani@gmail.com>
2014-12-03 15:47:18 -0300

    update version number

--- lib/M2XClient.m
+++ lib/M2XClient.m
@@ -17,7 +17,7 @@
 
 static NSString * const kDefaultApiBase = @"https://api-m2x.att.com";
 static NSString * const kDefaultApiVersion = @"v2";
-static NSString * const kLibVersion = @"2.0.0";
+static NSString * const kLibVersion = @"2.0.1";
 
 @implementation M2XClient
 
