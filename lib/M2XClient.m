13010c34dfedbd44c4149726ce310d2649a41d4f
Luis Floreani <luis.floreani@gmail.com>
2015-04-07 11:32:42 -0300

    Bump version to 2.1.0

--- lib/M2XClient.m
+++ lib/M2XClient.m
@@ -17,7 +17,7 @@
 
 static NSString * const kDefaultApiBase = @"https://api-m2x.att.com";
 static NSString * const kDefaultApiVersion = @"v2";
-static NSString * const kLibVersion = @"2.0.2";
+static NSString * const kLibVersion = @"2.1.0";
 
 @implementation M2XClient
 
