225aa512325cbbe051037b937f3e83bbaee2b774
nithin <nithinyell@gmail.com>
2016-10-19 21:47:09 -0700

    Revert "Updated the path and object declaration"
    This reverts commit 1aef9fc719625d42d3413ef1d4b292b650bc434b.

--- lib/M2XCollection.m
+++ lib/M2XCollection.m
@@ -36,23 +36,6 @@ static NSString * const kPath = @"/collections";
     }];
 }
 
-+ (void)addDeviceToTheCollection:(M2XClient *)client withDeviceId:(NSString *)deviceid  completionHandler:(M2XCollectionCallback)completionHandler{
-    NSString *pathByAppendingDeviceID = [NSString stringWithFormat:@"%@/devices/%@", kPath,deviceid];
-    [client putWithPath:[NSString stringWithFormat:@"%@", pathByAppendingDeviceID] parameters:nil completionHandler:^(M2XResponse *response) {
-        M2XCollection *collection = [[M2XCollection alloc] initWithClient:client attributes:response.json];
-        completionHandler(collection, response);
-    }];
-}
-
-+ (void)removeDeviceFromTheCollection:(M2XClient *)client withDeviceId:(NSString *)deviceid  completionHandler:(M2XCollectionCallback)completionHandler{
-    NSString *pathByAppendingDeviceID = [NSString stringWithFormat:@"%@/devices/%@", kPath,deviceid];
-    [client deleteWithPath:[NSString stringWithFormat:@"%@", pathByAppendingDeviceID] parameters:nil completionHandler:^(M2XResponse *response) {
-        M2XCollection *collection = [[M2XCollection alloc] initWithClient:client attributes:response.json];
-        completionHandler(collection, response);
-    }];
-    
-}
-
 #pragma mark - metadata
 
 - (void)metadataWithCompletionHandler:(M2XBaseCallback)completionHandler {
@@ -79,6 +62,19 @@ static NSString * const kPath = @"/collections";
     }];
 }
 
+- (void)addDeviceToTheCollection:(M2XClient *)client withDeviceId:(NSString *)deviceid  completionHandler:(M2XCollectionCallback)completionHandler{
+    NSString *pathByAppendingDeviceID = [NSString stringWithFormat:@"%@/devices/%@", [self path],deviceid];
+    [self.client putWithPath:[NSString stringWithFormat:@"%@", pathByAppendingDeviceID] parameters:nil completionHandler:^(M2XResponse *response) {
+        completionHandler(self, response);
+    }];
+}
 
+- (void)removeDeviceFromTheCollection:(M2XClient *)client withDeviceId:(NSString *)deviceid  completionHandler:(M2XCollectionCallback)completionHandler{
+    NSString *pathByAppendingDeviceID = [NSString stringWithFormat:@"%@/devices/%@", [self path],deviceid];
+    [self.client deleteWithPath:[NSString stringWithFormat:@"%@", pathByAppendingDeviceID] parameters:nil completionHandler:^(M2XResponse *response) {
+        completionHandler(self, response);
+    }];
+    
+}
 
 @end
